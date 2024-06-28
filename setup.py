2a0506bdded6dd575ebaafc012a683a810c98350
Chris Hirsch <chris@kiatek.com>
2024-06-27 22:02:01 -0600

    feat: 🎸 Initial commit after being moved out of yearly code

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(use_scm_version={"write_to": "botconfig/version.py"})
+
+#setup(name='hello',
+#      version='0.1',
+#      description='A plugin to say hello in several ways',
+#      url='http://ictshore.com/',
+#      author='Your Name',
+#      author_email='your.name@example.com',
+#      license='MIT',
+#      packages=['hello'],
+#      zip_safe=False)
